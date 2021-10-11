1b7535ef8111cd9982c37f1c00e8aeb4492c234a
QwiglyDee <qwiglydee@gmail.com>
2021-10-12 03:58:47 +0700

    upd templates

--- src/templates.mjs
+++ src/templates.mjs
@@ -1,24 +1,26 @@
 const _slot_re = new RegExp(/\$\{(\w+(\.\w+)*)\}/, 'g');
 
+
 export function tmpl_slots(tmpl) {
     return new Set(Array.from(tmpl.matchAll(_slot_re)).map(m => m[1]));
 }
 
-export function tmpl_paths(tmpl) {
-    return new Set(Array.from(tmpl_slots(tmpl)).map(slot => slot.split('.')));
+export function tmpl_deps(tmpl) {
+    return new Set(Array.from(tmpl_slots(tmpl)).map(slot => slot.split('.')[0]));
 }
 
-export function tmpl_deps(tmpl) {
-    return new Set(Array.from(tmpl_paths(tmpl)).map(path => path[0]));
+export function tmpl_path_extract(path, data) {
+    return path.split('.').reduce((o, k) => {
+        if (o === undefined || !(k in o)) return undefined;
+        return o[k];
+    }, data);
 }
 
 export function tmpl_interpolate(tmpl, context) {
-    function extract(path) {
-        return path.reduce((o, k) => {
-            if (!(k in o)) { throw new Error(`missing data '${k}' in '${path}'`)}
-            return o[k];
-        }, context);
+    function extract(p) {
+        let val = tmpl_path_extract(p, context);
+        if (val === undefined) throw new Error(`missing data path ${p}`);
+        return val;
     }
-
-    return tmpl.replaceAll(_slot_re, (m0, m1) => extract(m1.split('.')));
+    return tmpl.replaceAll(_slot_re, (m0, m1) => extract(m1, context));
 }
--- test/templates.test.js
+++ test/templates.test.js
@@ -1,13 +1,25 @@
 import { expect } from '@open-wc/testing';
 
-import { tmpl_slots, tmpl_paths, tmpl_deps, tmpl_interpolate } from "../src/templates";
+import { tmpl_slots, tmpl_deps, tmpl_path_extract, tmpl_interpolate } from "../src/templates";
 
 describe("templates", () => {
     const tmpl = '<div class="${foo.prop1} ${foo.prop2}" attr="${baz}">Text ${bar.prop.prop1}, ${bar.prop.prop2}, ${baz}</div>';
     const plain = "plain text";
+    const data = {
+        'foo': {
+            'prop1': "Foo_prop1",
+            'prop2': "Foo_prop2"
+        },
+        'bar': {
+            'prop': {
+                'prop1': "Bar_prop_prop1",
+                'prop2': "Bar_prop_prop2"
+            }
+        },
+        'baz': "Baz",
+    }
 
-
-    it("parse slots", () => {
+    it("can parse slots", () => {
         expect(tmpl_slots(tmpl)).to.deep.equal(new Set([
             'foo.prop1',
             'foo.prop2',
@@ -17,25 +29,11 @@ describe("templates", () => {
         ]));
     });
 
-    it("parse no slots", () => {
+    it("can parse no slots", () => {
         expect(tmpl_slots(plain)).to.be.empty;
     });
 
-    it("parse paths", () => {
-        expect(tmpl_paths(tmpl)).to.deep.equal(new Set([
-            ['foo', 'prop1'],
-            ['foo', 'prop2'],
-            ['bar', 'prop', 'prop1'],
-            ['bar', 'prop', 'prop2'],
-            ['baz']
-        ]));
-    });
-
-    it("parse no paths", () => {
-        expect(tmpl_paths(plain)).to.be.empty;
-    });
-
-    it("parse deps", () => {
+    it("can parse deps", () => {
         expect(tmpl_deps(tmpl)).to.deep.equal(new Set([
             'foo',
             'bar',
@@ -43,48 +41,31 @@ describe("templates", () => {
         ]));
     });
 
-    it("parse no deps", () => {
+    it("can parse no deps", () => {
         expect(tmpl_deps(plain)).to.be.empty;
     });
 
-    it("render", () => {
-        const data = {
-            'foo': {
-                'prop1': "Foo_prop1",
-                'prop2': "Foo_prop2"
-            },
-            'bar': {
-                'prop': {
-                    'prop1': "Bar_prop_prop1",
-                    'prop2': "Bar_prop_prop2"
-                }
-            },
-            'baz': "Baz",
-        }
-        let result = tmpl_interpolate(tmpl, data);
-
-        expect(result).to.equal('<div class="Foo_prop1 Foo_prop2" attr="Baz">Text Bar_prop_prop1, Bar_prop_prop2, Baz</div>');
+    it("can extract objs", () => {
+        expect(tmpl_path_extract("foo", data)).to.deep.equal({'prop1': "Foo_prop1", 'prop2': "Foo_prop2"});
     });
 
-    it("render plain", () => {
-        let result = tmpl_interpolate(plain, {});
-        expect(result).to.equal(plain);
+    it("can extract broken paths", () => {
+        expect(tmpl_path_extract("foo.broken", data)).to.be.undefined;
     });
 
-    it("fails to render missing obj", () => {
-        const tmpl = '<span>${baz}</span>';
-        function render() {
-            return tmpl_interpolate(tmpl, {foo: "Foo"});
-        }
-        expect(render).to.throw(Error);
+    it("can interpolate", () => {
+        expect(tmpl_interpolate(tmpl, data)).to.equal('<div class="Foo_prop1 Foo_prop2" attr="Baz">Text Bar_prop_prop1, Bar_prop_prop2, Baz</div>');
     });
 
-    it("fails to render missing prop", () => {
-        const tmpl = '<span>${foo.bar.baz}</span>';
-        function render() {
-            return tmpl_interpolate(tmpl, {foo: {}});
-        }
-        expect(render).to.throw(Error);
+    it("can interpolate plain", () => {
+        expect(tmpl_interpolate(plain, {})).to.equal(plain);
     });
 
+    it("fail to interpolate broken field", () => {
+        expect(() => tmpl_interpolate('<span>${baz}</span>', {})).to.throw(Error);
+    });
+
+    it("fail to interpolate broken path", () => {
+        expect(() => tmpl_interpolate('<span>${foo.bar.baz}</span>', {foo: {}})).to.throw(Error);
+    });
 })
