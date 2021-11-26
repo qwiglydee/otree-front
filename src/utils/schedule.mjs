de9173a371e8ea8fa14e3f589e5a6509918a973b
QwiglyDee <qwiglydee@gmail.com>
2021-11-26 17:57:14 +0700

    updated scheduler

--- src/utils/schedule.mjs
+++ src/utils/schedule.mjs
@@ -7,58 +7,86 @@ import { Timers } from "./timers";
  * - { time: number, display: string, input: bool, ...} -- a phase to happen at specified time in ms, and with given parameters
  * - { time, ..., timeout: number} -- fires timeout after defined phase
  * - { name: "name", ... } -- defines custom phase to trigger manually with .trigger("name")
+ * - a phase without time -- is a default phase to reset
  */
 export class Schedule {
   constructor(page, phases) {
     this.page = page;
-
-    this.validate(phases);
-    this.phases = phases;
-
     this._timers = new Timers();
 
-    this.init();
+    this.timed = null;
+    this.named = null;
+    this.default = null;
+    this.current = null;
+
+    this.init(phases);
+    this.setup();
+    this.reset();
   }
 
-  validate(phases) {
-    if (phases.filter(it => 'timeout' in it).length > 1) throw new Error("Multiple timeouts in phases");
-    if (phases.filter(it => it.input === true).length > 1) throw new Error("Multiple input phases not supported");
-    let named = phases.filter(it => 'name' in it);
-    let names = new Set(named.map(it => it.name));
-    if (named.length != names.size) throw new Error("Duplicated phase names");
-  } 
+  init(phases) {
+    let defaults = phases.filter((it) => !("name" in it) && !("time" in it));
+    if (defaults.length > 1) throw new Error("Duplicated default phases");
+    if (defaults.length == 1) this.default = defaults[0];
 
-  init() {
-    this.page.on('otree.time.phase', (event) => {
+    let named = phases.filter((it) => "name" in it),
+      names = new Set(named.map((it) => it.name));
+    if (named.length != names.size) throw new Error("Duplicated named phases");
+
+    this.named = new Map(named.map((it) => [it.name, it]));
+
+    this.timed = phases.filter((it) => "time" in it);
+
+    if (phases.filter((it) => "timeout" in it).length > 1)
+      throw new Error("Multiple timeouts in phases");
+    if (phases.filter((it) => it.input === true).length > 1)
+      throw new Error("Multiple input phases not supported");
+  }
+
+  setup() {
+    this.page.on("otree.time.phase", (event) => {
       if (event.detail.input) {
-        performance.mark('input');
+        performance.mark("input");
       }
-    })
-    this.page.on('otree.page.response', () => {
-      performance.mark('response');
-      performance.measure('reaction_time', 'input', 'response');
-    })
+    });
+
+    this.page.on("otree.page.response", () => {
+      performance.mark("response");
+      performance.measure("reaction_time", "input", "response");
+    });
   }
 
   run() {
     performance.clearMeasures();
     performance.clearMarks();
-    this.phases.forEach((phase, i) => {
-      this._timers.delay(`phase-${i}`, () => this.page.fire('otree.time.phase', phase), phase.time);
-      if ('timeout' in phase) {
-        this._timers.delay(`timeout`, () => {
-          this.page.fire('otree.time.out');
-          this._timers.cancel();
-        }, phase.time + phase.timeout);
+
+    this.timed.forEach((phase, i) => {
+      this._timers.delay(`phase-${i}`, () => this.toggle(phase), phase.time);
+      if ("timeout" in phase) {
+        this._timers.delay(`timeout`, () => this.timeout(), phase.time + phase.timeout);
       }
     });
   }
 
-  trigger(name) {
-    let m = this.phases.filter(item => item.name == name);
-    if (m.length == 0) throw new Error(`Phase ${name} not defined`);
-    let phase = m[0];
-    this.page.fire('otree.time.phase', phase);
+  toggle(phase) {
+    this.current = phase;
+    this.page.fire("otree.time.phase", phase);
+  }
+
+  timeout() {
+    this.current = null;
+    this.page.fire("otree.time.out");
+    this.cancel();
+  }
+
+  switch(name) {
+    this.toggle(this.named.get(name));
+  }
+
+  reset() {
+    if (this.default) {
+      this.toggle(this.default);
+    }
   }
 
   cancel() {
@@ -66,7 +94,7 @@ export class Schedule {
   }
 
   reaction_time() {
-    let m = performance.getEntriesByName('reaction_time').slice(-1);
+    let m = performance.getEntriesByName("reaction_time").slice(-1);
     if (m.length == 0) return undefined;
     return m[0].duration;
   }
--- test/utils/schedule.test.js
+++ test/utils/schedule.test.js
@@ -1,4 +1,4 @@
-import { expect, oneEvent, aTimeout, nextFrame  } from "@open-wc/testing";
+import { expect, oneEvent, aTimeout, nextFrame } from "@open-wc/testing";
 
 import { Page } from "../../src/page";
 import { Schedule } from "../../src/utils/schedule";
@@ -15,115 +15,170 @@ describe("schedule", () => {
     page = new Page(body);
   });
 
-  it("runs phases", async () => {
-    schedule = new Schedule(page, [
-      { time: 0, foo: "foo0" },
-      { time: 100, foo: "foo1" },
-      { time: 200, foo: "foo2" },
-    ]);
+  describe("validation", () => {
 
-    const t0 = Date.now();
-    schedule.run();
-
-    detail = await pageEvent("otree.time.phase");
-    expect(detail).to.eql({ time: 0, foo: "foo0" });
-    expect(Date.now() - t0).to.be.within(0, 10);
-
-    detail = await pageEvent("otree.time.phase");
-    expect(detail).to.eql({ time: 100, foo: "foo1" });
-    expect(Date.now() - t0).to.be.within(100, 110);
-
-    detail = await pageEvent("otree.time.phase");
-    expect(detail).to.eql({ time: 200, foo: "foo2" });
-    expect(Date.now() - t0).to.be.within(200, 210);
-  });
-
-  it("fails to take 2 timeouts", async () => {
-    expect(
-      () =>
-        new Schedule(page, [
-          { time: 100, foo: "foo1", timeout: 200 },
-          { time: 200, foo: "foo1", timeout: 200 },
-        ])
-    ).to.throw;
-  });
-
-  it("timeouts", async () => {
-    schedule = new Schedule(page, [{ time: 100, foo: "foo1", timeout: 200 }]);
-
-    const t0 = Date.now();
-    schedule.run();
-
-    detail = await pageEvent("otree.time.phase");
-    expect(detail).to.eql({ time: 100, foo: "foo1", timeout: 200 });
-    expect(Date.now() - t0).to.be.within(100, 110);
-
-    detail = await pageEvent("otree.time.out");
-    expect(detail).to.be.null;
-    expect(Date.now() - t0).to.be.within(300, 310);
-  });
-
-  it("cancels timers after timeout", async () => {
-    let counter = 0;
-
-    page.body.addEventListener("otree.time.phase", (e) => {
-      counter++;
+    it("disallows duplicate default", () => {
+      expect(() => new Schedule(page, [{ foo: "foo1" }, { foo: "foo2" }])).to.throw;
     });
 
-    schedule = new Schedule(page, [
-      { time: 100, foo: "foo1", timeout: 200 },
-      { time: 500, foo: "foo5" },
-    ]);
+    it("disallows duplicate input phases", () => {
+      expect(
+        () =>
+          new Schedule(page, [
+            { time: 0, input: true },
+            { time: 100, input: false },
+            { time: 200, input: true },
+          ])
+      ).to.throw;
+    });
 
-    const t0 = Date.now();
-    schedule.run();
+    it("disallows duplicate timeouts", () => {
+      expect(
+        () =>
+          new Schedule(page, [
+            { time: 100, foo: "foo1", timeout: 1000 },
+            { time: 200, foo: "foo2", timeout: 1000 },
+          ])
+      ).to.throw;
+    });
 
-    detail = await pageEvent("otree.time.phase");
-    expect(counter).to.eq(1);
-    expect(detail).to.eql({ time: 100, foo: "foo1", timeout: 200 });
-    expect(Date.now() - t0).to.be.within(100, 110);
-
-    detail = await pageEvent("otree.time.out");
-    expect(detail).to.be.null;
-    expect(Date.now() - t0).to.be.within(300, 310);
-
-    await aTimeout(1000);
-    expect(counter).to.eq(1);
+    it("disallows duplicate names", () => {
+      expect(
+        () =>
+          new Schedule(page, [
+            { name: "foo", foo: "foo1" },
+            { name: "foo", foo: "foo2" },
+          ])
+      ).to.throw;
+    });
   });
 
-  it("fails to take 2 named", async () => {
-    expect(
-      () =>
-        new Schedule(page, [
-          { name: "foo", foo: "foo1", timeout: 200 },
-          { name: "foo", foo: "foo1", timeout: 200 },
-        ])
-    ).to.throw;
-  });
+  describe("running", () => {
+    it("resets", async () => {
+      schedule = new Schedule(page, [{ foo: "foo0" }, { time: 100, foo: "foo1" }]);
 
-  it("triggers named phases", async () => {
-    schedule = new Schedule(page, [{ name: "foo", foo: "foo1" }]);
+      schedule.run();
 
-    schedule.trigger("foo");
-    detail = await pageEvent("otree.time.phase");
-    expect(detail).to.eql({ name: "foo", foo: "foo1" });
-  });
+      detail = await pageEvent("otree.time.phase");
+      expect(detail).to.eql({ foo: "foo0" });
 
-  it("measures reaction", async () => {
-    schedule = new Schedule(page, [{ time: 100, foo: "foo1", input: true }]);
-    
-    schedule.run();
-    
-    await pageEvent("otree.time.phase");
+      detail = await pageEvent("otree.time.phase");
+      expect(detail).to.eql({ time: 100, foo: "foo1" });
 
-    await aTimeout(200);
-    page.fire('otree.page.response');
+      schedule.reset();
+      detail = await pageEvent("otree.time.phase");
+      expect(detail).to.eql({ foo: "foo0" });
+    });
 
-    await pageEvent("otree.page.response");
+    it("runs phases", async () => {
+      schedule = new Schedule(page, [
+        { time: 0, foo: "foo0" },
+        { time: 100, foo: "foo1" },
+        { time: 200, foo: "foo2" },
+      ]);
 
-    let reaction = schedule.reaction_time();
+      const t0 = Date.now();
+      schedule.run();
 
-    expect(reaction).to.be.within(200, 210);
+      detail = await pageEvent("otree.time.phase");
+      expect(detail).to.eql({ time: 0, foo: "foo0" });
+      expect(Date.now() - t0).to.be.within(0, 10);
 
+      detail = await pageEvent("otree.time.phase");
+      expect(detail).to.eql({ time: 100, foo: "foo1" });
+      expect(Date.now() - t0).to.be.within(100, 110);
+
+      detail = await pageEvent("otree.time.phase");
+      expect(detail).to.eql({ time: 200, foo: "foo2" });
+      expect(Date.now() - t0).to.be.within(200, 210);
+    });
+
+    it("timeouts", async () => {
+      schedule = new Schedule(page, [
+        { time: 100, foo: "foo1", timeout: 500 },
+        { time: 200, foo: "foo2" },
+      ]);
+
+      const t0 = Date.now();
+      schedule.run();
+
+      detail = await pageEvent("otree.time.phase");
+      expect(detail).to.eql({ time: 100, foo: "foo1", timeout: 500 });
+
+      detail = await pageEvent("otree.time.phase");
+      expect(detail).to.eql({ time: 200, foo: "foo2" });
+
+      detail = await pageEvent("otree.time.out");
+      expect(detail).to.be.null;
+      expect(Date.now() - t0).to.be.within(600, 610);
+    });
+
+    it("cancels timers after timeout", async () => {
+      let counter = 0;
+
+      page.on("otree.time.phase", () => counter++);
+
+      schedule = new Schedule(page, [
+        { time: 100, foo: "foo1", timeout: 500 },
+        { time: 500, foo: "foo5" },
+        { time: 600, foo: "foo6" },
+      ]);
+
+      schedule.run();
+
+      detail = await pageEvent("otree.time.out");
+      expect(counter).to.eq(2);
+
+      await aTimeout(1000);
+      expect(counter).to.eq(2);
+    });
+
+    it("triggers named phases", async () => {
+      schedule = new Schedule(page, [
+        { name: "foo1", foo: "foo1" },
+        { name: "foo2", foo: "foo2" },
+      ]);
+
+      schedule.switch("foo1");
+      detail = await pageEvent("otree.time.phase");
+      expect(detail).to.eql({ name: "foo1", foo: "foo1" });
+
+      schedule.switch("foo2");
+      detail = await pageEvent("otree.time.phase");
+      expect(detail).to.eql({ name: "foo2", foo: "foo2" });
+    });
+
+    it("measures reaction", async () => {
+      schedule = new Schedule(page, [{ input: true }]);
+
+      schedule.run();
+
+      await aTimeout(100);
+      page.fire("otree.page.response");
+
+      await pageEvent("otree.page.response");
+
+      let reaction = schedule.reaction_time();
+
+      expect(reaction).to.be.within(100, 110);
+    });
+
+    it("measures multiresponse reaction", async () => {
+      schedule = new Schedule(page, [{ input: true }]);
+
+      schedule.run();
+
+      await aTimeout(100);
+      page.fire("otree.page.response");
+      await pageEvent("otree.page.response");
+
+      await aTimeout(200);
+      page.fire("otree.page.response");
+      await pageEvent("otree.page.response");
+
+      let reaction = schedule.reaction_time();
+
+      expect(reaction).to.be.within(300, 310);
+    });
   });
 });
